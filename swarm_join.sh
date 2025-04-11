ba1deef31e23f9a2cd0d14fa8727ae5f5f3d6f3d
rbq4ds <ltaquantum@gmail.com>
2025-04-11 12:40:51 +0200

    Update swarm_join.sh

--- swarm_join.sh
+++ swarm_join.sh
@@ -1,1 +1,2 @@
-    docker swarm join --token SWMTKN-1-5por4150023ugng7j95xpcoj6l50krgholpolcyqk4np1yitaf-2l1cbu6r7m4cps5pwegcq1acb 192.168.33.10:2377
+# Archivo de ejemplo que se creará automáticamente al crear swarm en manager y que cogerá el nodo1 con join y token
+#docker swarm join --token SWMTKN-1-5por4150023ugng7j95xpcoj6l50krgholpolcyqk4np1yitaf-2l1cbu6r7m4cps5pwegcq1acb 192.168.33.10:2377
